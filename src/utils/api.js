f751da12ceb019b900d80c1240dc92f962fad9d6
anynoise00 <anynoise00@gmail.com>
2023-06-20 18:51:55 -0300

    feat: Create API module and exported instance

---
+++ src/utils/api.js
@@ -0,0 +1,89 @@
+const api = new Api({
+  baseUrl: `https://around.nomoreparties.co/v1/${groupId}`,
+  headers: {
+    authorization: token,
+    'Content-Type': 'application/json',
+  },
+});
+
+export default api;
+
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  getUserData() {
+    return fetch(this._baseUrl + '/users/me', {
+      headers: this._headers,
+    }).then(checkResponse);
+  }
+
+  updateUserData({ name, about }) {
+    return fetch(this._baseUrl + '/users/me', {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name,
+        about,
+      }),
+    }).then(checkResponse);
+  }
+
+  updateAvatar({ avatar }) {
+    return fetch(this._baseUrl + '/users/me/avatar', {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar,
+      }),
+    }).then(checkResponse);
+  }
+
+  getInitialCards() {
+    return fetch(this._baseUrl + '/cards', {
+      headers: this._headers,
+    }).then(checkResponse);
+  }
+
+  addCard({ name, link }) {
+    return fetch(this._baseUrl + '/cards', {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        name,
+        link,
+      }),
+    }).then(checkResponse);
+  }
+
+  deleteCard(cardId) {
+    return fetch(this._baseUrl + '/cards/' + cardId, {
+      method: 'DELETE',
+      headers: this._headers,
+    }).then(checkResponse);
+  }
+
+  likeCard(cardId) {
+    return fetch(this._baseUrl + '/cards/likes/' + cardId, {
+      method: 'PUT',
+      headers: this._headers,
+    }).then(checkResponse);
+  }
+
+  dislikeCard(cardId) {
+    return fetch(this._baseUrl + '/cards/likes/' + cardId, {
+      method: 'DELETE',
+      headers: this._headers,
+    }).then(checkResponse);
+  }
+}
+
+function checkResponse(res) {
+  if (res.ok) {
+    return res.json();
+  }
+
+  return Promise.reject(`Error: ${res.status}`);
+}
